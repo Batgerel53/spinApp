894677af714071d819ff335ab22a17fb22283c68
batgerel53 <batgerelphone@gmail.com>
2021-09-06 16:00:52 +0800

    zasvar css

--- src/Form/TohirolType.php
+++ src/Form/TohirolType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Tohirol;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +14,12 @@ class TohirolType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-        
-
-
         $builder
-            ->add('name')
-            ->add('quantity')
-
-
+            ->add('name',      TextType::class, ['label'=>'Тохиролын нэр'])
+            ->add('quantity' , NumberType::class, ['label'=>'Тоо ширхэг'], [ 'empty_data' => 'Тоо ширхэг оруулна уу',])
             // ->add('chance')
-        
         ;
     }
-
 
     public function configureOptions(OptionsResolver $resolver)
     {
